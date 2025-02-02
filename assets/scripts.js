9646a65be4ec91c7c5cabb9f698a158e175f67e2
anishda <129377111+anishda@users.noreply.github.com>
2025-02-02 11:27:00 +0100

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const projects = document.querySelectorAll('.project');
     projects.forEach(project => {
         project.addEventListener('mouseover', function() {
-            project.style.backgroundColor = '#2e2e2e';
+            project.style.boxShadow = '0 6px 10px rgba(0, 0, 0, 0.15)';
         });
         project.addEventListener('mouseout', function() {
-            project.style.backgroundColor = '#1e1e1e';
+            project.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
         });
     });
 });
